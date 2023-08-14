23f1d3840aa7fe09260e1c44434102a8b50bed3d
Matthew McAuliffe <mcauliffem@icloud.com>
2023-08-14 17:23:59 -0400

    Altered models to accept user, cleared sample data in prep for the add entry function. Auth users will be used to login and register soon, so that the meals can be attributed and showed only to specific users.

--- incredible/meals/models.py
+++ incredible/meals/models.py
@@ -1,21 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    fname_text = models.CharField(max_length=200)
-    lname_text = models.CharField(max_length=200)
-    user_name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.user_name
-
 class Meal(models.Model):
-    entree_choice_text = models.CharField(max_length=200)
+    entree_choice_text = models.CharField(max_length=100)
     entree_rating = models.IntegerField(default=100)
-    notes = models.CharField(max_length=500, default="")
-    restaurant_name_text = models.CharField(max_length=200)
-    date = models.DateTimeField("Date dined")
-    # eaten_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    notes = models.TextField(default="")
+    restaurant_name_text = models.CharField(max_length=100)
+    date = models.DateTimeField("Date dined", default=timezone.now)
+    eaten_by = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         retstr = str(self.entree_rating) + "/100 for " + str(self.entree_choice_text) + " at " + str(self.restaurant_name_text) + " on " + str(self.date)
         return retstr
