e2b4d3091d462efb36876fc97a30b4c117f7ca16
Matthew McAuliffe <mcauliffem@icloud.com>
2023-08-15 19:46:48 -0400

    Created working registration page using crispy for formatting and messages and redirect on sucess to home screen. Split this code out into the users app.

--- incredible/incredible/urls.py
+++ incredible/incredible/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     # path("", include("meals.urls")),
     path("meals/", include("meals.urls")),
+    path("register/", user_views.register, name="register"),
     path('admin/', admin.site.urls),
 ]
